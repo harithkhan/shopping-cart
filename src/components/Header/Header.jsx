e75b17a6e9f6eb2cfd5a78cdb5ef2d82183d1534
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-05-22 15:39:20 +0800

    feat: add logo to homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
+import shopLogo from "../../assets/shop-logo.png";
+import styles from "./Header.module.css";
+
 function Header() {
     return (
         <>
+            <img src={shopLogo} alt="Shop Logo" className={styles.logo} />
             <div>This is a header</div>
         </>
     );
