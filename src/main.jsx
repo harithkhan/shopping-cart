7de36f48fa899f80ca61df2620e45bd07b36c85c
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-05-23 16:09:54 +0800

    style: add css reset code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
+import "./index.css";
 import routes from "./Routes";
 
 const router = createBrowserRouter(routes);
