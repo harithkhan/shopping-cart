e6e8c253de75ab40b72d0af448f30f758f7501da
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-05-23 20:42:33 +0800

    feat: add responsive dog photos for hero section

--- src/components/Main/Hero/Hero.jsx
+++ src/components/Main/Hero/Hero.jsx
@@ -1,6 +1,14 @@
+import styles from "./Hero.module.css";
+import phoneDog from "../../../assets/dog-hero-phone.png";
+import defaultDog from "../../../assets/dog-hero-desktop.png";
+
 function Hero() {
     return (
         <>
+            <picture>
+                <source srcSet={phoneDog} media="(max-width: 768px)" />
+                <img src={defaultDog} alt="Responsive Dog" />
+            </picture>
             <p>This is the hero section</p>
         </>
     );
