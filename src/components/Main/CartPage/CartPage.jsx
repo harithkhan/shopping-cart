eb05a79b1bb72a560994db59afb89d0ffa3e53f8
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-05-29 15:39:48 +0800

    feat: add empty cart display for empty cart

--- src/components/Main/CartPage/CartPage.jsx
+++ src/components/Main/CartPage/CartPage.jsx
@@ -8,32 +8,42 @@ function CartPage({ cart }) {
     const discountRate = 0.15;
     const discount = subtotal * discountRate;
     const delivery = 10;
-    const total = subtotal - discount + delivery;
+    const total = parseFloat((subtotal - discount + delivery).toFixed(2));
 
     return (
         <div className={styles.cartPage}>
-            <div className={styles.cartContainer}>
-                <h2 className={styles.cartHeader}>Your Cart</h2>
-                {Object.entries(cart).map(([id, item]) => (
-                    <div key={id} className={styles.cartItems}>
-                        <img
-                            src={item.product.image}
-                            alt={item.product.title}
-                            className={styles.productImage}
-                        />
-                        <p>{item.product.title}</p>
-                        <p>Quantity: {item.quantity}</p>
-                        <p>Price: RM{item.product.price}</p>
+            {Object.keys(cart).length === 0 ? (
+                <p>Your Cart is empty</p>
+            ) : (
+                <>
+                    <div className={styles.cartContainer}>
+                        <h2 className={styles.cartHeader}>Your Cart</h2>
+                        {Object.entries(cart).map(([id, item]) => (
+                            <div key={id} className={styles.cartItems}>
+                                <img
+                                    src={item.product.image}
+                                    alt={item.product.title}
+                                    className={styles.productImage}
+                                />
+                                <p>{item.product.title}</p>
+                                <p>Quantity: {item.quantity}</p>
+                                <p>Price: RM{item.product.price}</p>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div className={styles.summaryContainere}>
-                <h3 className={styles.summaryHeader}>Your Summary</h3>
-                <p className={styles.subTotal}>Subtotal: RM{subtotal}</p>
-                <p className={styles.discount}>Discount: 15%</p>
-                <p className={styles.deliveryFees}>Delivery Fees: RM10</p>
-                <p className={styles.total}>Total: RM{total}</p>
-            </div>
+                    <div className={styles.summaryContainere}>
+                        <h3 className={styles.summaryHeader}>Your Summary</h3>
+                        <p className={styles.subTotal}>
+                            Subtotal: RM{subtotal}
+                        </p>
+                        <p className={styles.discount}>Discount: 15%</p>
+                        <p className={styles.deliveryFees}>
+                            Delivery Fees: RM10
+                        </p>
+                        <p className={styles.total}>Total: RM{total}</p>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
