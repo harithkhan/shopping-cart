29e93b6dc19db4f851224202d8cb3a8ddc6b9ce4
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-05-29 15:07:33 +0800

    feat: add scaffolding for cart page

--- src/components/Main/CartPage/CartPage.jsx
+++ src/components/Main/CartPage/CartPage.jsx
@@ -1,10 +1,39 @@
 import styles from "./CartPage.module.css";
 
 function CartPage({ cart }) {
-    console.log(cart);
+    const subtotal = Object.values(cart).reduce((acc, item) => {
+        return acc + item.product.price * item.quantity;
+    }, 0);
+
+    const discountRate = 0.15;
+    const discount = subtotal * discountRate;
+    const delivery = 10;
+    const total = subtotal - discount + delivery;
+
     return (
-        <div className={styles.cartContainer}>
-            <p>This is just a test cart.</p>
+        <div className={styles.cartPage}>
+            <div className={styles.cartContainer}>
+                <h2 className={styles.cartHeader}>Your Cart</h2>
+                {Object.entries(cart).map(([id, item]) => (
+                    <div key={id} className={styles.cartItems}>
+                        <img
+                            src={item.product.image}
+                            alt={item.product.title}
+                            className={styles.productImage}
+                        />
+                        <p>{item.product.title}</p>
+                        <p>Quantity: {item.quantity}</p>
+                        <p>Price: RM{item.product.price}</p>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.summaryContainere}>
+                <h3 className={styles.summaryHeader}>Your Summary</h3>
+                <p className={styles.subTotal}>Subtotal: RM{subtotal}</p>
+                <p className={styles.discount}>Discount: 15%</p>
+                <p className={styles.deliveryFees}>Delivery Fees: RM10</p>
+                <p className={styles.total}>Total: RM{total}</p>
+            </div>
         </div>
     );
 }
