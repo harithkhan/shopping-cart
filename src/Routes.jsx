1f57d7005a0b864dfd547dc601b68484d28036be
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-05-29 14:17:31 +0800

    feat: add CartPage component to route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,6 +2,7 @@ import App from "./App";
 import Shop from "./components/Main/Shop/Shop";
 import Product from "./components/Main/Product/Product";
 import Hero from "./components/Main/Hero/Hero";
+import CartPage from "./components/Main/CartPage/CartPage";
 
 const routes = [
     {
@@ -25,6 +26,14 @@ const routes = [
         element: (
             <App>
                 <Product />
+            </App>
+        ),
+    },
+    {
+        path: "/cart",
+        element: (
+            <App>
+                <CartPage />
             </App>
         ),
     },
---
+++ src/components/Main/CartPage/CartPage.jsx
@@ -0,0 +1,11 @@
+import styles from "./CartPage.module.css";
+
+function CartPage({ cart }) {
+    return (
+        <div className={styles.cartContainer}>
+            <p>This is just a test cart.</p>
+        </div>
+    );
+}
+
+export default CartPage;
